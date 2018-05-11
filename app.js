6d3e19f0e8658c264e5dcdac0131e78a6ebf68e5
dudzio <dud2iol@gmail.com>
2018-05-11 23:36:49 +0200

    user model added, libraries imported

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require("express"),
 	app = express(),
-	mongoose = require("mongoose");
+	mongoose = require("mongoose"),
+	passport = require("passport"),
+	bodyParser = require("body-parser"),
+	LocalStrategy = require("passport-local"),
+	passportLocalMongoose = require("passport-local-mongoose"),
+	User = require("./models/user");
 
 mongoose.connect("mongodb://localhost/auth_demo");
 app.set("view engine", "ejs");
