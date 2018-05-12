7cb4d36cf21b8c2191b27fa681c56d15090b8ced
dudzio <dud2iol@gmail.com>
2018-05-12 16:59:38 +0200

    isLogged in function added, middleware to secret page added

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(passport.initialize());
 app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -31,7 +33,7 @@ app.get("/", function(req, res){
 	res.render("home");
 });
 
-app.get("/secret", function(req, res){
+app.get("/secret", isLoggedIn, function(req, res){
 	res.render("secret");
 })
 
@@ -69,6 +71,18 @@ app.post("/login", passport.authenticate("local", {
 	failureRedirect: "/login"
 }), function(req, res){
 });
+
+app.get("/logout", function(req, res){
+	req.logout();
+	res.redirect("/");
+});
+
+function isLoggedIn(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+}
 
 app.listen(3000, function(){
 	console.log("Server started");
